b663921bc45a18892931355216762d824d3dfcba
kordimion <kordimion@gmail.com>
2020-12-30 20:06:44 +0300

    Project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { Button, Typography } from "@material-ui/core";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant='h3' gutterBottom>
+        Hello world!
+      </Typography>
+      <Button color="primary" variant="contained">Click</Button>
+    </div>
+  );
+}
+
+export default App;
