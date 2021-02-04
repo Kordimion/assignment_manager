369b1bc9bd3ea1c721129f6965a599ee5d03a6de
kordimion <kordimion@gmail.com>
2021-02-04 08:22:16 +0300

    added top navbar to the application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { Grid } from "@material-ui/core";
 import Assignments from "./Assignments";
+import Navbar from "./Navbar";
 
 function App() {
   
   return (
     <div className="App">
       <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Navbar />
+        </Grid>
         <Grid item xs={12}>
           <Assignments />
         </Grid>
