7212b91661dc779eb72421ac7b321d102582329d
kordimion <kordimion@gmail.com>
2021-02-05 18:06:30 +0300

    filled sidebar with content, required for further development

--- src/components/SidebarContent.jsx
+++ src/components/SidebarContent.jsx
@@ -3,13 +3,24 @@ import React from 'react'
 import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper';
-
+import Typography from '@material-ui/core/Typography';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         minWidth: "200px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "stretch"
     },
+    divider: {
+        marginTop: `${theme.spacing(1)}px`,
+        marginBottom: `${theme.spacing(1)}px`,
+    },
+    button: {
+
+    }
 }));
 
 function SidebarContent() {
@@ -17,9 +28,18 @@ function SidebarContent() {
 
     return (
         <Paper className={classes.root}> 
-            <Button variant="contained" color="primary">Inbox</Button>
-            <Divider />
-            <Button variant="contained" color="secondary">Other</Button>
+            <Button size="large" variant="contained" color="primary"  className={classes.button}>Inbox</Button>
+            <Divider className={classes.divider} />
+            <Typography variant="h5" align="center">Lessons</Typography>
+            <ButtonGroup
+                orientation="vertical"
+                aria-label="vertical contained button group"
+                variant="contained"
+            >
+                <Button size="large" align="left">Math</Button>
+                <Button size="large">Physics</Button>
+                <Button size="large">Chemistry</Button>
+            </ButtonGroup>
         </Paper>
     )
 }
