0ae9fd488c6e3ef7561ba3cee895d84620b56f5f
kordimion <kordimion@gmail.com>
2021-02-05 17:10:15 +0300

    created a structure for sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,34 +6,51 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/icons/Menu';
-
+import SidebarContent from './SidebarContent';
+import Drawer from '@material-ui/core/Drawer'
 import { makeStyles } from '@material-ui/core/styles';
+import Hidden from '@material-ui/core/Hidden';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     loginBtn: {
         marginRight: "1rem",
     },
     title: {
         flexGrow: 1,
     }
+}))
 
-})
 
 function Navbar() {
     const classes = useStyles();
+    const [isOpen, setIsOpen] = useState(false); 
 
+    const toggleDrawer = openFlag => event => {
+        if(event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) return;
+        
+        setIsOpen(openFlag);
+    }
+    
+    
     return (
-        <AppBar position="static">
-        <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-            <Menu />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-            Assignments Manager
-            </Typography>
-            <Button color="inherit" className={classes.loginBtn}>Login</Button>
-        </Toolbar>
-        </AppBar>
+        <React.Fragment>
+            <AppBar position="static">
+            <Toolbar>
+                <Hidden smUp>
+                    <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+                        <Menu />
+                    </IconButton>
+                </Hidden>
+                <Typography variant="h6" className={classes.title}>
+                Assignments Manager
+                </Typography>
+                <Button color="inherit" className={classes.loginBtn}>Login</Button>
+            </Toolbar>
+            </AppBar>
+            <Drawer anchor="left" open={isOpen} onClose={ toggleDrawer(false) }>
+                <SidebarContent />
+            </Drawer>
+        </React.Fragment>
     )
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,14 +3,25 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import Navbar from '../components/Navbar';
 import Assignments from '../components/Assignments';
+import Hidden from '@material-ui/core/Hidden';
+import SidebarContent from '../components/SidebarContent';
+import Paper from '@material-ui/core/Paper';
 
 function MainPage() {
     return (
-        <Grid container spacing={2}>
+        <Grid container spacing={1} alignItems="stretch">
             <Grid item xs={12}>
                 <Navbar />
             </Grid>
-            <Grid item xs={12}>
+            <Hidden xsDown>
+                <Grid item xs={3}>
+                    <Paper>
+                        <SidebarContent />
+                    </Paper>
+                </Grid>
+            </Hidden>
+            
+            <Grid item xs={12} sm={9}>
                 <Assignments />
             </Grid>
         </Grid>
